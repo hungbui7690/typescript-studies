46604001e91aff3b7686661e83986fb0a136c215
hungbui7690 <hungbui7690@gmail.com>
2023-03-19 03:22:52 -0600

    What / Why / Installation / How to Run

---
+++ playground/index.js
@@ -0,0 +1,20 @@
+/*
+  What / Why
+  > pic
+
+  Installation
+  > npm install typescript --save-dev
+  > npm install -g typescript
+    > either one is ok > after this, we can use "tsc" command
+
+  Playground
+  > pic
+
+  Running Typescript File
+  > tsc index.ts
+    > will create index.js file > compile ts code into js
+
+
+
+*/
+console.log('Hello World');
