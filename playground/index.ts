26c143c46be4cb1c5e0662cfe2c055ea643c2f11
hungbui7690 <hungbui7690@gmail.com>
2023-03-19 03:27:49 -0600

    Our First Type Annotation

--- playground/index.ts
+++ playground/index.ts
@@ -1,19 +1,13 @@
 /*
-  What / Why
-  > pic
+  Our First Type Annotation
+  > pic: type-annotation
 
-  Installation
-  > npm install typescript --save-dev
-  > npm install -g typescript
-    > either one is ok > after this, we can use "tsc" command
-
-  Playground
-  > pic
-
-  Running Typescript File
-  > tsc index.ts
-    > will create index.js file > compile ts code into js 
+  (***) when we use movieTitle. > it will show just string methods since we define this variable as string
 
 */
 
-console.log('Hello World')
+
+let movieTitle: string = 'Amadeus'
+movieTitle = 'Arrivals'
+movieTitle = 9 // complain
+movieTitle.
