ec09bea4f6ea95e969908a1686d80e1a0926e06a
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 13:04:04 +0700

    Creating Type Aliases P1

--- playground/index.ts
+++ playground/index.ts
@@ -1,12 +1,21 @@
 /*
-  Excess Properties P2
-  - if we do like below, ts will only check if that object has those 2 properties or not 
+  Creating Type Aliases P1
+  - pic: type-alias
+  - this is basically just a way to reuse the object definition
 
+  > in the below example, we use { x: number; y: number } many times 
+    > and the code below looks so nasty
 */
 
-function printName(person: { first: string; last: string }): void {
-  console.log(`${person.first} ${person.last}`)
+let coordinate: { x: number; y: number } = { x: 32, y: 2 }
+
+function randomCoordinate(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() }
 }
 
-const singer = { first: 'Mick', last: 'Jagger', age: 18 } // create an object
-printName(singer) // no err now if we pass the object variable
+function doublePoint(point: { x: number; y: number }): {
+  x: number
+  y: number
+} {
+  return { x: point.x * 2, y: point.y * 2 }
+}
