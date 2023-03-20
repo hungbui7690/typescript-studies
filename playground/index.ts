ebb1d516a3deb8c0ea7e1b2ae604a16319ae66a7
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 14:16:28 +0700

    Optional Properties

--- playground/index.ts
+++ playground/index.ts
@@ -1,43 +1,14 @@
 /*
-  Nested Objects
-  - pic
+  Optional Properties
+  > ?: 
   
-  > tsc index.ts
-  > node index.js
 */
 
-// assume we work on a project for Spotify
-type Song = {
-  title: string
-  artist: string
-  numStreams: number
-  credits: {
-    producer: string
-    writer: string
-  }
+type Point = {
+  x: number
+  y: number
+  z?: number // (***) z is optional
 }
 
-function calculatePayout(song: Song): number {
-  return 0.0033 * song.numStreams
-}
-
-function printSong(song: Song): void {
-  console.log(`${song.title} - ${song.artist}`)
-}
-
-//////////////////////////////////////
-
-const unchained = {
-  title: 'Unchained Melody',
-  artist: 'Brothers',
-  numStreams: 123885483,
-  credits: {
-    producer: 'Phil Jones',
-    writer: 'Phoenix ',
-  },
-}
-
-const earning = calculatePayout(unchained)
-console.log(earning)
-
-printSong(unchained)
+const myPointX: Point = { x: 1, y: 2, z: 3 }
+const myPointY: Point = { x: 1, y: 2 } // doesn't need to have z
