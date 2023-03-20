2c72c499a0c72b42f88d6139b1bd7c96a575af9a
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 21:23:11 +0700

    Introducing Enums P1

--- playground/index.ts
+++ playground/index.ts
@@ -1,23 +1,12 @@
 /*
-  A Bit More On Tuples
-  - we should use object for this example 
+  Introducing Enums P1
+  - pic
+  - use when we work with sets of value many times 
+
+  > UP/DOWN/LEFT/RIGHT > gaming
+  > PENDING/SHIPPED/DELIVERED
 */
 
-type HTTPResponse = [number, string]
-
-let res: HTTPResponse = [200, 'OK']
-res = [404, 'NOT FOUND']
-// res[0] = "OK" // complain
-
-res.push(123) // (***) but the problem appears when we have the correct shape, then we push in > no complain
-res.pop()
-res.pop()
-res.pop()
-res.pop()
-res.pop()
-res.pop() // we also can pop() many times > this is the limitation of tuples
-
-const responses: HTTPResponse[] = [
-  [404, 'NOT FOUND'],
-  [200, 'OK'],
-]
+const PENDING = 0
+const SHIPPED = 1
+const DELIVERED = 2
