57542a2fbb67eed4a7a06471b98cdc977a8dcdc2
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 02:30:52 +0700

    The Lib Compiler Option P2

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,16 +1,10 @@
 /*
-  The Lib Compiler Option P1
-  - pic
-  - "lib": [] > by default, TS knows about DOM 
-    > if we uncomment, the list is empty > we need to add DOM to the list
-  
-  
+  The Lib Compiler Option P2
 
 */
 
-// hover onto "document" > type = Document object > JS knows about this type > right click: Type Definition > or Ctrl + click on "document"
 const btn = document.querySelector('.btn')
 console.log(btn)
 
-// right now, TS does not know about this method > comes from later JS version
-'hello'.replaceAll()
+// (***) we add ES2021 to the list of lib in tsconfig
+'hello'.replaceAll('l', '!')
