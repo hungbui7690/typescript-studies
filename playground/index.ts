641bb34773088dd2ef3c18211ccf5784439eb708
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 15:29:28 +0700

    Type Narrowing w Union Types P1

--- playground/index.ts
+++ playground/index.ts
@@ -1,33 +1,12 @@
 /*
-  Introducing Union Types
-  - pic
-  - multiple types
+  Type Narrowing w Union Types P1
+  - 
 
 */
 
-let age: number | string = 21
-age += 1
-age = '24'
-
-/////////////////////////////////////
-
-type Point = {
-  x: number
-  y: number
+function printAge(age: number | string) {
+  console.log(`You are ${age} years old`)
 }
 
-type Loc = {
-  lat: number
-  lng: number
-}
-
-// (***)
-let coord: Point | Loc = {
-  x: 1,
-  y: 20,
-}
-
-coord = {
-  lat: 324.123,
-  lng: 123.213,
-}
+printAge(23)
+printAge('27')
