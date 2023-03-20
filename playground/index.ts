ed62b7f40c50ce36bea44b3f0955006b6d249ae7
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 11:37:49 +0700

    Return Type Annotations P1

--- playground/index.ts
+++ playground/index.ts
@@ -1,12 +1,21 @@
 /*
-  Working With Default Parameters
+  Return Type Annotations P1
+  - pic: function-return-type
 
 */
 
-// default parameter
-function greet(person: string = 'stranger') {
+// in this case, ts infers the return type for us
+function square(num: number) {
+  return num * num
+}
+
+// infer return type as string
+function greet(person: string) {
   return `Hi there, ${person}`
 }
 
-greet()
-greet('Joe')
+// (***) this case, we forget to have the return keyword > but ts doesn't catch it
+function add(a: number, b: number) {
+  a + b
+}
+add(1, 2)
