ac0c87c09fe31cbd37b0d99ce48672c6038b3b9b
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 14:24:05 +0700

    Object Types EXERCISE

--- playground/index.ts
+++ playground/index.ts
@@ -1,25 +1,35 @@
 /*
-  Intersection Types P2
-  - combine types
+  Object Types EXERCISE
 
 */
 
-type Cat = {
-  numLives: number
+// Write the Movie type alias to make the following two variables properly typed
+// Make sure that "originalTitle" is optional and "title" is readonly
+const dune: Movie = {
+  title: 'Dune',
+  originalTitle: 'Dune Part One',
+  director: 'Denis Villeneuve',
+  releaseYear: 2021,
+  boxOffice: {
+    budget: 165000000,
+    grossUS: 108327830,
+    grossWorldwide: 400671789,
+  },
 }
 
-type Dog = {
-  breed: string
+const cats: Movie = {
+  title: 'Cats',
+  director: 'Tom Hooper',
+  releaseYear: 2019,
+  boxOffice: {
+    budget: 95000000,
+    grossUS: 27166770,
+    grossWorldwide: 73833348,
+  },
 }
 
-// combine + add on
-type CatDog = Cat &
-  Dog & {
-    age: number
-  }
+// Write a function called getProfit that accepts a single Movie object
+// It should return the movie's worldwide gross minus its budget
 
-const bic = {
-  numLives: 9,
-  breed: 'short-hair',
-  age: 4,
-}
+// For example...
+// getProfit(cats) => -21166652
