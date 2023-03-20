81f6c9406b953f84fe0e0a76f72880f45cc8ccca
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 03:54:53 +0700

    Building The Todo List P2

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,19 +1,21 @@
 "use strict";
 /*
-  Building The Todo List P1
-  - index.html > add ul
+  Building The Todo List P2
+  - we want to add a checkbox next to each todo
 
 */
 const form = document.querySelector('form');
 const btn = document.querySelector('.btn');
 const input = document.getElementById('todo-input');
-const list = document.querySelector('ul'); // (1)
+const list = document.querySelector('ul');
 function handleSubmit(e) {
     e.preventDefault();
-    // (2)
     const newToto = input.value;
     const newLI = document.createElement('li');
+    const checkbox = document.createElement('input'); // (1)
+    checkbox.type = 'checkbox';
     newLI.textContent = newToto;
+    newLI.append(checkbox); // (2)
     list === null || list === void 0 ? void 0 : list.append(newLI);
     input.value = '';
 }
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,21 +1,25 @@
 /*
-  Building The Todo List P1
-  - index.html > add ul
+  Building The Todo List P2
+  - we want to add a checkbox next to each todo
 
 */
 
 const form = document.querySelector('form')!
 const btn = document.querySelector('.btn')! as HTMLButtonElement
 const input = document.getElementById('todo-input')! as HTMLInputElement
-const list = document.querySelector('ul') // (1)
+const list = document.querySelector('ul')
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault()
 
-  // (2)
   const newToto = input.value
   const newLI = document.createElement('li')
+
+  const checkbox = document.createElement('input') // (1)
+  checkbox.type = 'checkbox'
+
   newLI.textContent = newToto
+  newLI.append(checkbox) // (2)
   list?.append(newLI)
   input.value = ''
 }
