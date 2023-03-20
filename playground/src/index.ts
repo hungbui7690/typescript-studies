aa99476937a5c47f040c4ffb25a7c86f8e23431f
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 02:37:43 +0700

    TypeScripts NonNull Assertion Operator P2

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,15 +1,16 @@
 /*
-  TypeScripts NonNull Assertion Operator P1
-  - below, we have the button selected, but what is the type of this button
-    > hover: Element | null > generic type
-    > console.dir(btn) > scroll to the end, we will see HtmlButtonElement
-    
+  TypeScripts NonNull Assertion Operator P2
+  - we have couple of solutions: 
+    + Method 1: optional chaining > btn?.addEventListener()
+    + Method 2: NonNull Assertion Operator
+      > next lecture
   
 
 */
 
-// when we select, ts knows nothing about this element > it just know that we use the querySelector(str) and receive an Element or null
 const btn = document.querySelector('.btn')
 
-// btn could be null
-btn.addEventListener('click', () => {})
+// optional chaining
+btn?.addEventListener('click', () => {
+  alert('CLICK !!!')
+})
