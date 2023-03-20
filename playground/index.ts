eeda7ed5a292017193dff4fe963138beb6cb7c8d
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 11:49:17 +0700

    Anonymous Function Contextual Typing

--- playground/index.ts
+++ playground/index.ts
@@ -1,19 +1,13 @@
 /*
-  Return Type Annotations P2
+  Anonymous Function Contextual Typing
+  - pic: function-anonymous-function
 
 */
 
-// we have to add return type > so that if we forget return keyword, ts will notify us
-function add(a: number, b: number): number {
-  a + b
-}
-add(1, 2)
+const colors = ['red', 'orange', 'yellow']
 
-// hover : string | number > ts is smart
-function random(num: number) {
-  if (Math.random() < 0.5) {
-    return num.toString()
-  }
-
-  return num
-}
+// ts infers and knows right away that "color" === string
+colors.map((color) => {
+  color.toUpperCase()
+  color.toFixed()
+})
