bc47237c22ae515fe8191ae9996abc95a3410cd8
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 11:46:24 +0700

    Return Type Annotations P2

--- playground/index.ts
+++ playground/index.ts
@@ -1,21 +1,19 @@
 /*
-  Return Type Annotations P1
-  - pic: function-return-type
+  Return Type Annotations P2
 
 */
 
-// in this case, ts infers the return type for us
-function square(num: number) {
-  return num * num
-}
-
-// infer return type as string
-function greet(person: string) {
-  return `Hi there, ${person}`
-}
-
-// (***) this case, we forget to have the return keyword > but ts doesn't catch it
-function add(a: number, b: number) {
+// we have to add return type > so that if we forget return keyword, ts will notify us
+function add(a: number, b: number): number {
   a + b
 }
 add(1, 2)
+
+// hover : string | number > ts is smart
+function random(num: number) {
+  if (Math.random() < 0.5) {
+    return num.toString()
+  }
+
+  return num
+}
