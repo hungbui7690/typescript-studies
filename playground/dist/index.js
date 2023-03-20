ad1e93adc2d00f9efcb38fc3684cc8a0ddf54ef5
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 02:14:50 +0700

    A Simple Project Setup

---
+++ playground/dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+/*
+  A Simple Project Setup
+  
+  (1) tsc --init
+  
+  (2)
+  - create src/ + dist/
+  - config: outDir > dist + include > src folder
+
+  (3)
+  > tsc -w
+
+  (4)
+  - create html file and link to dist folder
+  - open with live server
+    > or we can use lite-server
+    > npm init -y
+    > npm install lite-server
+    > setup script in package.json
+        "start": "lite-server"
+
+*/
+function printDouble(msg) {
+    console.log(msg);
+    console.log(msg);
+}
+printDouble('Hello World');
