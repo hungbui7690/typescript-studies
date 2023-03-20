839acdf96d850166f1e5124535765c4293132799
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 23:39:43 +0700

    Outdir Option

---
+++ playground/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+  Outdir Option
+  - pic
+  - output directory
+  - create dist/
+
+  
+  
+*/
+const norma = {
+    breed: 'Silk',
+    eggsPerWeek: 4,
+    name: 'Norma',
+    age: 2,
+};
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,12 +1,10 @@
 /*
-  Include & Exclude Options P2
-  - put dontTouch into src/
-  - "exclude" will filter based on "include" setting
-  
-  
-  - check pic: we can see that by default, exclude will exclude node_modules 
-  > but if we have exclude setting > we need to add "node_modules" to exclude settings as well
-      "exclude": ["dontTouch.ts", "node_modules"]
+  Outdir Option
+  - pic
+  - output directory 
+  - create dist/ 
+
+      "outDir": "./dist" 
   
 */
 
