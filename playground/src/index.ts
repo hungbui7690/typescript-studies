ca94f46b5a293e01477273d24b45f5fd0b7c76a3
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 02:29:01 +0700

    The Lib Compiler Option P1

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,12 +1,16 @@
 /*
-  Working With The DOM
-  - index.html > add button
+  The Lib Compiler Option P1
+  - pic
+  - "lib": [] > by default, TS knows about DOM 
+    > if we uncomment, the list is empty > we need to add DOM to the list
   
-  - console.dir(document) > there are many things in this document variable 
-    > WHAT TYPE OF THIS ????
-    
+  
+
 */
 
 // hover onto "document" > type = Document object > JS knows about this type > right click: Type Definition > or Ctrl + click on "document"
 const btn = document.querySelector('.btn')
 console.log(btn)
+
+// right now, TS does not know about this method > comes from later JS version
+'hello'.replaceAll()
