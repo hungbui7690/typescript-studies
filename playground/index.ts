e7619911d44d031648f27817eb7277569d765b45
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 14:47:04 +0700

    More Array Syntax P1

--- playground/index.ts
+++ playground/index.ts
@@ -1,17 +1,9 @@
 /*
-  Working With Array Types P2
-  - pic 
+  More Array Syntax P1
+  - this syntax is used for other things beside arrays, though it can be used with array
 
 */
 
-// with this definition, we can have this array empty, or we can add items to this array
-const activeUsers: string[] = []
-activeUsers.push('joe')
-activeUsers.push('tony')
+const boolListX: Array<boolean> = []
 
-// array of numbers
-const ageList: number[] = [45, 14, 77]
-ageList[0] = 99
-
-// array of boolean
-const boolList: boolean[] = [true, true, false]
+const boolListY: boolean[] = []
