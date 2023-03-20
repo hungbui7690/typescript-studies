770340140cf99f6b894317d5fa09750ccd45e97b
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 04:55:57 +0700

    JS - Class Fields P2

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,25 +1,31 @@
 /*
-  JS - Class Fields P1
-  - nowadays, we don't use this way > next lecture
+  JS - Class Fields P2
 
 */
 
 class Player {
+  // (***) write here
+  score = 0
+  numLives = 10
+
   constructor(first, last) {
     this.first = first
     this.last = last
-
-    // (***) traditional way
-    this.score = 0
-    this.numLives = 10
   }
 
   taunt() {
     console.log('Booya!!')
   }
+
+  // (***)
+  loseLife() {
+    this.numLives--
+  }
 }
 
 const player = new Player('Bic', 'Babon')
 player.taunt()
-console.log(player.first)
-console.log(player.last)
+console.log(player)
+
+player.loseLife() // (***)
+console.log(player)
