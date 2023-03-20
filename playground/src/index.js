0fd5a26cddab185208218a7c824b78218ae4f4ef
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 04:46:13 +0700

    JS - Constructors P1

--- playground/src/index.js
+++ playground/src/index.js
@@ -1,14 +1,14 @@
 /*
-  JS - The Class Keyword
-  - pic
-  - interface: describe the shape of an object 
-    > we don't initiate from interface > but we can create instance from a class
-
-  (***) we will work with .js file in these videos 
+  JS - Constructors P1
 
 */
 
 class Player {
+  // (***) will be call every time we instantiate a new instance
+  constructor() {
+    console.log('CONSTRUCTOR')
+  }
+
   taunt() {
     console.log('Booya!!')
   }
