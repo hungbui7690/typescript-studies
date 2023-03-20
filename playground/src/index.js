a8341131daa5ca20122c1ffaf5cdcb4900865011
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 23:30:44 +0700

    Include & Exclude Options P1

---
+++ playground/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+  Include & Exclude Options
+  - pic
+
+
+*/
+const norma = {
+    breed: 'Silk',
+    eggsPerWeek: 4,
+    name: 'Norma',
+    age: 2,
+};
---
+++ playground/src/index.ts
@@ -0,0 +1,24 @@
+/*
+  Include & Exclude Options P1
+  - pic
+  - put index + farmstand into src folder    
+
+  "include": ["src"]
+
+
+
+*/
+
+interface Chicken {
+  breed: string
+  eggsPerWeek: number
+  name: string
+  age: number
+}
+
+const norma: Chicken = {
+  breed: 'Silk',
+  eggsPerWeek: 4,
+  name: 'Norma',
+  age: 2,
+}
