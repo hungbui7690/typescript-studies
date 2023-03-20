c2eb8ae27f6a00c54dc95d804a35fcb0c544815a
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 12:15:00 +0700

    Function Types SOLUTION

--- playground/index.ts
+++ playground/index.ts
@@ -1,11 +1,11 @@
 /*
-  Function Types EXERCISE
+  Function Types SOLUTION
 
 */
 
-// **********************************************
-// ******************* PART 1 *******************
-// **********************************************
+///////////////////////////////////////////
+// PART 1
+///////////////////////////////////////////
 // Write a function called "twoFer" that accepts a person's name
 // It should return a string in the format "one for <name>, one for me"
 // If no name is provided, it should default to "you"
@@ -13,9 +13,15 @@
 // twoFer() => "One for you, one for me"
 // twoFer("Elton") => "One for Elton, one for me"
 
-// **********************************************
-// ******************* PART 2 *******************
-// **********************************************
+const twoFer = (name: string = 'you'): string => {
+  return `one for ${name}, one for me`
+}
+console.log(twoFer())
+console.log(twoFer('Elton'))
+
+///////////////////////////////////////////
+// PART 2
+///////////////////////////////////////////
 // Write a isLeapyear() function that accepts a year and returns true/false depending on if the year is a leap year
 // isLeapYear(2012) => true
 // isLeapYear(2013) => false
@@ -26,3 +32,11 @@
 // OR...
 // - year is a multiple of 400
 // hint - use modulo
+
+const isLeapYear = (year: number): boolean => {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) return true
+
+  return false
+}
+console.log(isLeapYear(2012))
+console.log(isLeapYear(2013))
