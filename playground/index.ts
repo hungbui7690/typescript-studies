468e30360c66db3af935bd4e89bff0602537577f
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 11:56:05 +0700

    The Never Type

--- playground/index.ts
+++ playground/index.ts
@@ -1,10 +1,23 @@
 /*
-  The Void Type
-  - pic: function-void-type
-  - functions don't return anything
+  The Never Type
+  - pic: functions-never-type
+  - function should never return 
+    + function may throw exception > no return 
+    + function can continuously runs in some sort of loops > never finishes 
+    > both functions above never have a chance to return anything 
+
+  (***) compare to void > void is technically a value > void in TS actually return "undefined" 
 
 */
 
-function printTwice(msg: string): void {
-  console.log(msg)
+// case 1
+function makeError(msg: string): never {
+  throw new Error(msg)
+}
+
+// case 2
+function gameLoop(): never {
+  while (true) {
+    console.log('Game Loop is running...')
+  }
 }
