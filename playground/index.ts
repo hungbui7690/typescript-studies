8466c4c3847e46d9dde18a20b52d95c9ecf6074c
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 21:14:45 +0700

    Introducing Tuples

--- playground/index.ts
+++ playground/index.ts
@@ -1,84 +1,13 @@
 /*
-  Union Types SOLUTION
-  
+  Introducing Tuples
+  - pic
+  - fixed length, fixed order (first item must be string, 2nd item must be boolean...)
+
 */
 
-//////////////////////////////////////
-// PART 1
-//////////////////////////////////////
-// Create a variable called highScore that can be a number OR a boolean
-let highScore: number | boolean = 9
+// array of multiple type
+const stuff: (string | number)[] = [1, 'joe', 'nick', 4]
 
-//////////////////////////////////////
-// PART 2
-//////////////////////////////////////
-// create an array called stuff
-// it can be an array of numbers OR an array of strings
-// it cannot be an array of numbers and strings (mixed together)
-let stuff: number[] | string[] = [1, 2, 3]
-stuff = ['nick', 'joe']
-// stuff = ['nick', 12]
-
-//////////////////////////////////////
-// PART 3
-//////////////////////////////////////
-// Create a literal type called SkillLevel
-// There are 4 allowed values: "Beginner", "Intermediate", "Advanced", and "Expert"
-type SkillLevel = 'Beginner' | 'Intermediate' | 'Advanced' | 'Expert'
-
-//////////////////////////////////////
-// PART 4
-//////////////////////////////////////
-// Create a type called SkiSchoolStudent
-// name must be a string
-// age must be a number
-// sport must be "ski" or "snowboard"
-// level must be a value from the SkillLevel type (from above)
-type SkiSchoolStudent = {
-  name: string
-  age: number
-  sport: 'ski' | 'snowboard'
-  level: SkillLevel
-}
-
-//////////////////////////////////////
-// PART 5
-//////////////////////////////////////
-// Define a type to represent an RGB color
-// r should be a number
-// g should be a number
-// b should be a number
-type RGBType = {
-  r: number
-  g: number
-  b: number
-}
-
-// Define a type to represent an HSL color
-// h should be a number
-// s should be a number
-// l should be a number
-type HSLType = {
-  h: number
-  s: number
-  l: number
-}
-
-// Create an array called colors that can hold a mixture of RGB and HSL color types
-let colors: (RGBType | HSLType)[] = []
-
-//////////////////////////////////////
-// PART 6
-//////////////////////////////////////
-// Write a function called greet that accepts a single string OR an array of strings
-// It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
-const greet = (name: string | string[]): void => {
-  if (typeof name === 'string') console.log(`Hello, ${name}`)
-  else {
-    name.forEach((n) => {
-      console.log(`Hello, ${n}`)
-    })
-  }
-}
-greet('joe')
-greet(['nick', 'john'])
+// if we want to have an array that represent RBG color > must have exact 3 items in the array > tuple
+let color: [number, number, number] = [0, 233, 255]
+color = [0, 0, 255, 0] // 4 elements > complain
