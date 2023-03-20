84c77f9dd55ec09f3802c66d86290642e1c2fb2e
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 02:35:31 +0700

    TypeScripts NonNull Assertion Operator P1

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,9 +1,14 @@
 "use strict";
 /*
-  The Lib Compiler Option P2
+  TypeScripts NonNull Assertion Operator P1
+  - below, we have the button selected, but what is the type of this button
+    > hover: Element | null > generic type
+    > console.dir(btn) > scroll to the end, we will see HtmlButtonElement
+    
+  
 
 */
+// when we select, ts knows nothing about this element > it just know that we use the querySelector(str) and receive an Element or null
 const btn = document.querySelector('.btn');
-console.log(btn);
-// (***) we add ES2021 to the list of lib in tsconfig
-'hello'.replaceAll('l', '!');
+// btn could be null
+btn.addEventListener('click', () => { });
