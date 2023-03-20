53a6a604fbfaed2de19edf8e1d4a63318b0328b3
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 22:34:19 +0700

    Interfaces Vs Type Aliases

--- playground/index.ts
+++ playground/index.ts
@@ -1,27 +1,30 @@
 /*
-  Interface - Multiple Inheritance
+  Interfaces Vs Type Aliases
+  - pic
+  - they both describe the object type
+
+  - Interface: 
+    + can reopen to add properties 
+    + can extends 
+    + must be object 
+        interface X = {}
+
+  - Type Alias
+    + interface cannot do this, since it must be object > type alias can use for multiple things
+        type X = "red" | "green"
+    + cannot reopen
+    + cannot use extends > need to use intersection
   
+  (***) object: use interface > other things: use type alias
 */
 
-interface Person {
+/////////////////////////////////////////
+type Color = 'red' | 'green' | 'blue'
+
+type Name = {
   name: string
 }
 
-interface Employee {
-  readonly id: number
-  email: string
-}
-
-// here
-interface Engineer extends Person, Employee {
-  level: string
-  languages: string[]
-}
-
-const pierre: Engineer = {
-  name: 'Pierre',
-  id: 5452,
-  email: 'pierre@gmail.com',
-  level: 'Senior',
-  languages: ['JS', 'Python'],
+type Person = Name & {
+  age: number
 }
