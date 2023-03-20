f759da13c978d6c3e142fb6c70959b36513d4840
hungbui7690 <hungbui7690@gmail.com>
2023-03-20 13:06:18 +0700

    Creating Type Aliases P2

--- playground/index.ts
+++ playground/index.ts
@@ -1,21 +1,24 @@
 /*
-  Creating Type Aliases P1
+  Creating Type Aliases P2
   - pic: type-alias
-  - this is basically just a way to reuse the object definition
-
-  > in the below example, we use { x: number; y: number } many times 
-    > and the code below looks so nasty
+  
 */
 
-let coordinate: { x: number; y: number } = { x: 32, y: 2 }
+// (***) define
+type Point = {
+  x: number
+  y: number
+}
 
-function randomCoordinate(): { x: number; y: number } {
+// use
+let coordinate: Point = { x: 32, y: 2 }
+
+// use
+function randomCoordinate(): Point {
   return { x: Math.random(), y: Math.random() }
 }
 
-function doublePoint(point: { x: number; y: number }): {
-  x: number
-  y: number
-} {
+// use
+function doublePoint(point: Point): Point {
   return { x: point.x * 2, y: point.y * 2 }
 }
