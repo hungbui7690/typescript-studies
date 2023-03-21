496ebee5230b06984d6defaa4ada41acd709d69c
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 20:51:07 +0700

    Installing Types Separately P1

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,26 +1,9 @@
 "use strict";
 /*
-  Working With Axios Types P4
-  - now, we will get list of Users
-    > https://jsonplaceholder.typicode.com/users
+  Installing Types Separately P1
+  - we will work with lodash, which does not come with Type Declaration File
+    > npm i --save lodash
 
+  - check github of lodash > package.json does not contain any info about type declaration files (unlike axios)
 */
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-// T = User[]
-axios_1.default
-    .get('https://jsonplaceholder.typicode.com/users')
-    .then((res) => {
-    const { data } = res;
-    data.forEach(printUser); // (***)
-})
-    .catch((e) => console.log(e));
-function printUser(user) {
-    console.log(user.name);
-    console.log(user.email);
-    console.log(user.phone);
-    console.log('***********');
-}
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,48 +1,9 @@
 /*
-  Working With Axios Types P4
-  - now, we will get list of Users
-    > https://jsonplaceholder.typicode.com/users
+  Installing Types Separately P1
+  - we will work with lodash, which does not come with Type Declaration File
+    > npm i --save lodash
 
+  - check github of lodash > package.json does not contain any info about type declaration files (unlike axios)
 */
 
-import axios from 'axios'
-
-interface User {
-  id: number
-  name: string
-  username: string
-  email: string
-  address: {
-    street: string
-    suite: string
-    city: string
-    zipcode: string
-    geo: {
-      lat: string
-      lng: string
-    }
-  }
-  phone: string
-  website: string
-  company: {
-    name: string
-    catchPhrase: string
-    bs: string
-  }
-}
-
-// T = User[]
-axios
-  .get<User[]>('https://jsonplaceholder.typicode.com/users')
-  .then((res) => {
-    const { data } = res
-    data.forEach(printUser) // (***)
-  })
-  .catch((e) => console.log(e))
-
-function printUser(user: User) {
-  console.log(user.name)
-  console.log(user.email)
-  console.log(user.phone)
-  console.log('***********')
-}
+import _ from 'lodash' // red line > could not find declaration files for lodash
