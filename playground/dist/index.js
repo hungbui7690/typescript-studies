4b1f0eee945949a92f8ca2880ff59a736bcf5602
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 07:28:50 +0700

    Introducing Generics P1

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,1 +1,11 @@
 "use strict";
+/*
+  Introducing Generics P1
+  - pic
+
+*/
+// generic is more generic than this
+function doThing(thing) {
+    return 1;
+}
+// with generic, we can define a function which receive types, and return exactly the type it received > accept string, then return string > accept number, then return number
---
+++ playground/src/index.ts
@@ -0,0 +1,12 @@
+/*
+  Introducing Generics P1
+  - pic 
+
+*/
+
+// generic is more generic than this
+function doThing(thing: number | string): number | string {
+  return 1
+}
+
+// with generic, we can define a function which receive types, and return exactly the type it received > accept string, then return string > accept number, then return number
