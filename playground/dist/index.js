0b3d7cfd3668385b7c39ae7d6cb4cf3530621a51
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 20:39:24 +0700

    Working With Axios Types P2

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,14 +1,28 @@
 "use strict";
 /*
-  Working With Axios Types P1
+  Working With Axios Types P2
   - https://jsonplaceholder.typicode.com/users/1
+
+
+  - check index.d.ts, we can see that AxiosResponse.data has type of T
+      get<T = any, R = AxiosResponse<T>, D = any>(url: string, config?:
+
+      export interface AxiosResponse<T = any, D = any> {
+      data: T;
+      status: number;
+      statusText: string;
+      headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
+      config: InternalAxiosRequestConfig<D>;
+      request?: any;
+    }
+
 */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
-// hover > we can see that this method return a promise
+// we set the type of T = boolean > hover: we will see the response type = boolean
 axios_1.default
     .get('https://jsonplaceholder.typicode.com/users/1')
     .then((res) => {
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,13 +1,27 @@
 /*
-  Working With Axios Types P1
+  Working With Axios Types P2
   - https://jsonplaceholder.typicode.com/users/1
+
+
+  - check index.d.ts, we can see that AxiosResponse.data has type of T
+      get<T = any, R = AxiosResponse<T>, D = any>(url: string, config?: 
+
+      export interface AxiosResponse<T = any, D = any> {
+      data: T;
+      status: number;
+      statusText: string;
+      headers: RawAxiosResponseHeaders | AxiosResponseHeaders;
+      config: InternalAxiosRequestConfig<D>;
+      request?: any;
+    }
+
 */
 
 import axios from 'axios'
 
-// hover > we can see that this method return a promise
+// we set the type of T = boolean > hover: we will see the response type = boolean
 axios
-  .get('https://jsonplaceholder.typicode.com/users/1')
+  .get<boolean>('https://jsonplaceholder.typicode.com/users/1')
   .then((res) => {
     console.log(res.data)
   })
