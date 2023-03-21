9c48ea2a0c5ad5a1c861818fda2f33a772bf10aa
hungbui7690 <hungbui7690@gmail.com>
2023-03-21 11:15:30 +0700

    Typeof Guards P2

--- playground/dist/index.js
+++ playground/dist/index.js
@@ -1,14 +1,11 @@
 "use strict";
 /*
-  Typeof Guards P1
-  - pic
-  - narrow to precise type
-    > ex: union to more precise type
-
+  Typeof Guards P2
 
 */
-// we can't do either of these
 function triple(value) {
-    return value * 3; // string cannot do this
-    return value.repeat(3); // number cannot do this
+    if (typeof value === 'string') {
+        return value.repeat(3);
+    }
+    return value * 3;
 }
--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,14 +1,12 @@
 /*
-  Typeof Guards P1
-  - pic
-  - narrow to precise type 
-    > ex: union to more precise type
-
+  Typeof Guards P2
 
 */
 
-// we can't do either of these
 function triple(value: number | string) {
-  return value * 3 // string cannot do this
-  return value.repeat(3) // number cannot do this
+  if (typeof value === 'string') {
+    return value.repeat(3)
+  }
+
+  return value * 3
 }
