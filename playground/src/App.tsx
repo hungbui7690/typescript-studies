96a903a86cb9e8c5a0d52f22b70ab54f97c998d7
hungbui7690 <hungbui7690@gmail.com>
2023-03-22 02:35:32 +0700

    React & TypeScript Introduction

---
+++ playground/src/App.tsx
@@ -0,0 +1,21 @@
+/*
+  React & TypeScript Introduction
+  - pic
+  - npx create-react-app client --template typescript
+
+  - there also tsconfig.json file
+'
+*/
+
+import React from 'react'
+import './App.css'
+
+// (***)
+const num: number = 99
+console.log(`Hello World, ${num}`)
+
+function App() {
+  return <div className='App'>App.tsx</div>
+}
+
+export default App
